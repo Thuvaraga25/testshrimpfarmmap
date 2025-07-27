f68ac8ce6b8ba25775355075ea1104e513692f5f
Thuvaraga25 <thuvaragathiru25@gmail.com>
2025-07-27 11:10:26 +0530

    Update Mangroves_4.js

--- layers/Mangroves_4.js
+++ layers/Mangroves_4.js
@@ -1,15 +1,1 @@
-var size = 0;
-var placement = 'point';
 
-var style_Mangroves_4 = function(feature, resolution){
-    return [ new ol.style.Style({
-        fill: new ol.style.Fill({
-            color: 'rgba(34, 139, 34, 0.5)'  // Semi-transparent forest green
-        }),
-        stroke: new ol.style.Stroke({
-            color: '#006400',               // Dark green stroke
-            width: 2                        // Thicker for better visibility
-        }),
-        text: createTextStyle(feature, resolution, "", "10px", "#000000", placement)
-    })];
-};
